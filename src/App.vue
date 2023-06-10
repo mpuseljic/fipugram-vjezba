82094c1490e6113da18160ad3e9279d29d0abdbe
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-06-10 22:34:56 +0200

    login,logout edit

--- src/App.vue
+++ src/App.vue
@@ -1,120 +1,124 @@
 <template>
-  <div id="app">
-    <nav id="nav" class="navbar navbar-expand-md navbar-light">
-      <router-link to="/" class="navbar-brand">
-        <img
-          src="@/assets/fipu_logo.png"
-          height="40"
-          class="d-inline-block align-top"
-          alt=""
-          loading="lazy"
-        />
-      </router-link>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarToggler"
-        aria-controls="navbarToggler"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarToggler">
-        <form id="search" class="navbar-form form-inline ml-auto">
-          <input
-            v-model="store.searchTerm"
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Pretraga"
-            aria-label="Search"
-          />
-        </form>
-        <!-- Image and text -->
-        <ul class="navbar-nav ml-auto">
-          <li v-if="!store.currentUser" class="nav-item">
-            <router-link to="/login" class="nav-link">Login</router-link>
-          </li>
-          <li v-if="!store.currentUser" class="nav-item">
-            <router-link to="/signup" class="nav-link">Sign up</router-link>
-          </li>
-          <li v-if="store.currentUser" class="nav-item">
-            <a href="#" @click.prevent="logout()" class="nav-link">Logout</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div id="app">
+        <nav id="nav" class="navbar navbar-expand-md navbar-light">
+            <router-link to="/" class="navbar-brand">
+                <img src="@/assets/fipu_logo.png" height="40" class="d-inline-block align-top" alt="" loading="lazy" />
+            </router-link>
+            <button
+                class="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="#navbarToggler"
+                aria-controls="navbarToggler"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarToggler">
+                <form id="search" class="navbar-form form-inline ml-auto">
+                    <input
+                        v-model="store.searchTerm"
+                        class="form-control mr-sm-2"
+                        type="search"
+                        placeholder="Pretraga"
+                        aria-label="Search"
+                    />
+                </form>
+                <!-- Image and text -->
+                <ul class="navbar-nav ml-auto">
+                    <li v-if="!store.currentUser" class="nav-item">
+                        <router-link to="/login" class="nav-link">Login</router-link>
+                    </li>
+                    <li v-if="!store.currentUser" class="nav-item">
+                        <router-link to="/signup" class="nav-link">Sign up</router-link>
+                    </li>
+                    <li v-if="store.currentUser" class="nav-item">
+                        <a href="#" @click.prevent="logout()" class="nav-link">Logout</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
 
-    <div class="container">
-      <router-view />
-      <!-- tu router dinamicki ubaci screen -->
+        <div class="container">
+            <router-view />
+            <!-- tu router dinamicki ubaci screen -->
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
-import store from "@/store";
-import { firebase } from "./firebase";
-import router from "@/router";
+import store from '@/store';
+import { firebase } from '@/firebase';
+import router from '@/router';
 
 firebase.auth().onAuthStateChanged((user) => {
-  if (user) {
-    console.log("*** " + user.email);
-    store.currentUser = user.email;
-  } else {
-    console.log("no user");
-    store.currentUser = null;
+    const currentRoute = router.currentRoute;
 
-    if (router.name != "login") {
-      router.push({ name: "login" });
+    //console.log('PROVJERA STANJA LOGINA!');
+    if (user) {
+        // User is signed in.
+        console.log('*** User', user.email);
+        store.currentUser = user.email;
+
+        if (!currentRoute.meta.needsUser) {
+            router.push({ name: 'home' });
+        }
+    } else {
+        // User is not signed in.
+        console.log('*** No user');
+        store.currentUser = null;
+
+        if (currentRoute.meta.needsUser) {
+            router.push({ name: 'login' });
+        }
     }
-  }
 });
 
 export default {
-  name: "app",
-  data() {
-    return {
-      store,
-    };
-  },
-  methods: {
-    logout() {
-      firebase
-        .auth()
-        .signOut()
-        .then(() => {
-          this.$router.push({ name: "login" });
-        });
+    name: 'app',
+    data() {
+        return {
+            store,
+        };
     },
-  },
+    methods: {
+        logout() {
+            firebase
+                .auth()
+                .signOut()
+                .then(() => {
+                    this.$router.push({ name: 'login' });
+                });
+        },
+    },
 };
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
     color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
+
+#nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
+}
+
 #search {
-  display: block;
-  text-align: center;
+    display: block;
+    text-align: center;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,58 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import store from "@/store";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: "/",
+    name: "home",
+    component: HomeView,
+    meta: {
+      needsUser: true,
+    },
   },
   {
-    path: '/login',
-    name: 'login',
+    path: "/login",
+    name: "login",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Login.vue"),
   },
   {
-    path: '/signup',
-    name: 'signup',
+    path: "/signup",
+    name: "signup",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/Signup.vue')
-  }
-]
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/Signup.vue"),
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  console.log(
+    "Stara ruta ",
+    from.name,
+    " nova ruta",
+    to.name,
+    "korisnik",
+    store.currentUser
+  );
+
+  const noUser = store.currentUser === null;
+
+  if (noUser && to.meta.needsUser) {
+    next('login')
+  } else {
+    next();
+  }
+});
+
+export default router;
