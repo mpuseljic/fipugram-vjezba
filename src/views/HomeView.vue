45afc17710c874c0f38a9637199580e30f2361a5
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-06-10 19:00:46 +0200

    vjezba

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,19 +4,27 @@
       <instagram-card />
       <instagram-card />
     </div>
-    <div class="col-4">Sidebar</div>
+    <div class="col-4">Sidebar {{ hello }} {{ lista }}</div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import InstagramCard from "@/components/InstagramCard.vue";
+
+let lista = [1,2,3,4,"ij"]
 
 export default {
   name: "HomeView",
+  data(){
+    return{
+      //kljuc vrijednost
+      hello: "world",
+      lista
+
+    }
+  },
   components: {
-    HelloWorld,
     InstagramCard,
   },
 };
