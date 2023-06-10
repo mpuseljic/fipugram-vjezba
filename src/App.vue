642d7a0552f92adb41efd933ba63041840e9bf89
Mirna Pušeljić <puseljicmirna@gmail.com>
2023-06-10 18:38:14 +0200

    login

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/login">Login</router-link>
+    </div>
+    <router-view />
+  </div>
 </template>
 
 <style lang="scss">
